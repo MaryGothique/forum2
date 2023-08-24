bdf24079a23da17803c8d5b0cab9cbae23247762
MaryGothique <user.email>
2023-08-24 14:35:20 +0200

    partie front prête

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\LoginType;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils, UserRepository  $repo): Response
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
@@ -28,13 +29,25 @@ class SecurityController extends AbstractController
         $form = $this->createForm(LoginType::class, $user);
         $form->handleRequest($request);
 
+// verifier si l utilisateur existe dans la base de donnés 
+//si oui alors je verifie s'il est le bon mdp 
+// si tous ça est "oui" je peux rediriger l utilisateur dans la page d' accueil connecté 
+
+/*
+public function findBy(array $criteria, ?array $orderBy = null, $limit = null, $offset = null)
+{
+    $repo = $this->User->findByUser()->getUser($this->userEntity);
+
+    return $repo->loadAll($criteria, $orderBy, $limit, $offset);
+}
+*/
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername, 
             'error' => $error,
             'login' => $form->createView(),
         ]);
     }
-
+       
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
