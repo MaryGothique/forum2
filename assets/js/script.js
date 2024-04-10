8c0fdee117597bdc8b5e0561cc86e66bda75f39f
MaryGothique <marypolly88@gmail.com>
2024-04-10 14:05:50 +0200

    adding diaporama

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+class Diaporama {
+    constructor(containerId) {
+        this.container = document.getElementById(containerId);
+        this.btnPrevious = this.container.querySelector(".previous");
+        this.btnNext = this.container.querySelector(".next");
+        this.images = Array.from(this.container.querySelectorAll("figure"));
+        this.index = 0;
+        this.affichage();
+        this.btnNext.addEventListener("click", () => { this.next() });
+        this.btnPrevious.addEventListener("click", () => { this.previous() });
+    }
+
+    next() {
+        this.index = (this.index + 1) % this.images.length;
+        this.affichage();
+    }
+
+    previous() {
+        this.index = (this.index - 1 + this.images.length) % this.images.length;
+        this.affichage();
+    }
+
+    affichage() {
+        this.images.forEach(image => image.style.display = 'none');
+        this.images[this.index].style.display = 'block';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const diaporama = new Diaporama("container");
+});
